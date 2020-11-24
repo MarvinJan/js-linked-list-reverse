6091722ac1b5bcf5d9ee52f06477e3f2c73e993f
MarvinJan <marvinjan@mail.ru>
2020-11-24 22:41:37 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-2ikp4y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-2ikp4y)
---
+++ index.ts
@@ -0,0 +1,63 @@
+// Import stylesheets
+import "./style.css";
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+class LinkedList {
+  head: Node;
+  constructor(nodes: Node[]) {
+    this.head = nodes[0];
+    nodes.reduce((prev, next) => (prev.next = next));
+  }
+
+  outputList() {
+    let curr = this.head;
+    let output: string = "";
+
+    while (curr) {
+      const str = `{ ${curr.value} } ${curr.next ? " => " : ""}`;
+      console.log(str);
+      output += str;
+      curr = curr.next;
+    }
+
+    const p = document.createElement("p");
+    p.append(output);
+    appDiv.append(p);
+  }
+
+  reverseList() {
+    let prev = this.head;
+    let curr = this.head.next;
+    prev.next = null;
+
+    while (true) {
+      let currCopy = curr;
+      curr = curr.next;
+      currCopy.next = prev;
+      prev = currCopy;
+
+      if (!curr) {
+        this.head = currCopy;
+        break;
+      }
+    }
+
+    this.outputList();
+  }
+}
+class Node {
+  next: Node;
+  value: number;
+
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+const list = new LinkedList([0, 1, 2, 3, 4].map(val => new Node(val)));
+list.reverseList();
+list.reverseList();
+list.reverseList();
